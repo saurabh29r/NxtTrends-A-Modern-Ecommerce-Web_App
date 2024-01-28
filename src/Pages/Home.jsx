42ce5920255b1d148be1585f379781561a94ba3e
Saurabh <saurabh100rab002@gmail.com>
2024-01-28 15:38:47 +0530

    dev modes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbars from "../Components/Navbars";
 import { Container, Row, Col } from "react-bootstrap";
 
-
 function Home() {
   return (
     <>
@@ -22,12 +21,7 @@ function Home() {
             </div>
             <div>
               <p> This is home page </p>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Blanditiis laudantium ex, quasi nihil, eligendi neque
-                perferendis adipisci quia doloremque optio recusandae similique
-                illum nisi provident quos maiores sequi maxime? Molestias.
-              </p>
+              <p>Lorem ipsum dolo es sequi maxime? Molestias.</p>
             </div>
             <div>
               <p> This is home page </p>
