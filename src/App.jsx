dca097b75d36cd0112a68d6ffde45b6929ffcc4b
Saurabh <saurabh100rab002@gmail.com>
2024-02-17 19:13:50 +0530

    added some card responsivness

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./Components/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-// import Page404 from "./Components/page404";
 import Carts from "./Pages/Carts";
 import Products from "./Pages/Products";
 import Protected from "./Components/Protected";
@@ -18,7 +17,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/carts" element={<Protected Comp={Carts} />} />
           <Route path="/products" element={<Protected Comp={Products} />} />
-          {/* <Route path="/*" element={<Page404 />} /> */}
           <Route path="/products/:id" element={<ProductsDetailPage />} />
         </Routes>
       </BrowserRouter>
